6d04083aa528ab3bcb4a8870a90fc749755bd722
Kunal <kunal123452003@gmail.com>
2024-03-28 22:29:16 +0530

    File 5 commited

---
+++ File 5/if.py
@@ -0,0 +1,9 @@
+for i in range(1,11):
+   if i == 1:
+       print("This is 1")
+   elif i==2:
+       print("This is 2")
+   elif i==6:
+        print("This is 6")
+   else:
+        print("This is ",i)
